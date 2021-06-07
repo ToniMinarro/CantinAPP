1187e37b46b50d1398ca97fa2402f2bfb03eb60e
ToniMinarro <antonio_jose91@hotmail.es>
2021-06-07 21:36:20 +0200

    Commit de prueba

--- php/Cantina.php
+++ php/Cantina.php
@@ -205,7 +205,7 @@ if(isset($_POST['newComposicion']))
 	<div id="insertaComposicion" class="panel">
 		<div class="row">
 			<div class="col-md-12">
-				<h2 class="text-center">Insertar nueva Composición</h2>
+				<h2 class="text-center">Insertar nuevo plato</h2>
 			</div>
 		</div>
 		<div class="col-md-12">
