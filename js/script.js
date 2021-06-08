fc50dcf6d5151dd48d351b2c73037b43c8576424
ToniMinarro <antonio_jose91@hotmail.es>
2021-06-08 12:31:21 +0200

    Cambios para RPI Deploy

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 const $ = document.querySelector.bind(document)
 const $$ = document.querySelectorAll.bind(document)
-const INDICE = 'http://localhost/'; 				// PARA DEPLOY EN LOCAL
-// const INDICE = 'http://rpi/'; 					// PARA DEPLOY EN RASPBERRY PI
+// const INDICE = 'http://localhost/'; 				// PARA DEPLOY EN LOCAL
+const INDICE = 'http://rpi/CantinAPP/'; 					// PARA DEPLOY EN RASPBERRY PI
 // const INDICE = 'http://www.rpi.fsalzillo.com/'; 	// PARA DEPLOY EN LAN
 // const INDICE = 'http://cantinapp.duckdns.org/'; 	// PARA DEPLOY EN WAN
 
